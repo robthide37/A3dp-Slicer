209eda75a04ef99c4f610a888709d8aa89721714
Filip Sykala <filip.sykala@prusa3d.cz>
2021-12-03 17:14:51 +0100

    remove unnecessary getting of raw pointer from unique pointer

--- src/slic3r/GUI/Gizmos/GLGizmosManager.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmosManager.cpp
@@ -1219,31 +1219,38 @@ bool GLGizmosManager::activate_gizmo(EType type)
     if (m_gizmos.empty() || m_current == type)
         return true;
 
-    GLGizmoBase* old_gizmo = m_current == Undefined ? nullptr : m_gizmos[m_current].get();
-    GLGizmoBase* new_gizmo = type == Undefined ? nullptr : m_gizmos[type].get();
-
-    if (old_gizmo) {
-        old_gizmo->set_state(GLGizmoBase::Off);
-        if (old_gizmo->get_state() != GLGizmoBase::Off)
+    if (m_current != Undefined) {
+        // clean up previous gizmo
+        GLGizmoBase &old_gizmo = *m_gizmos[m_current];
+        old_gizmo.set_state(GLGizmoBase::Off);
+        if (old_gizmo.get_state() != GLGizmoBase::Off)
             return false; // gizmo refused to be turned off, do nothing.
 
-        if (! m_parent.get_gizmos_manager().is_serializing()
-         && old_gizmo->wants_enter_leave_snapshots())
+        if (!m_serializing
+         && old_gizmo.wants_enter_leave_snapshots())
             Plater::TakeSnapshot snapshot(wxGetApp().plater(),
-                old_gizmo->get_gizmo_leaving_text(),
+                old_gizmo.get_gizmo_leaving_text(),
                 UndoRedo::SnapshotType::LeavingGizmoWithAction);
     }
 
-    if (new_gizmo && ! m_parent.get_gizmos_manager().is_serializing()
-     && new_gizmo->wants_enter_leave_snapshots())
+    // check deactivation of gizmo
+    if (type == Undefined) { 
+        m_current = type;
+        return true;
+    }
+
+    // set up new gizmo
+    GLGizmoBase& new_gizmo = *m_gizmos[type];
+    if (!m_serializing && new_gizmo.wants_enter_leave_snapshots())
         Plater::TakeSnapshot snapshot(wxGetApp().plater(),
-            new_gizmo->get_gizmo_entering_text(),
+            new_gizmo.get_gizmo_entering_text(),
             UndoRedo::SnapshotType::EnteringGizmo);
 
     m_current = type;
+    new_gizmo.set_state(GLGizmoBase::On);
+    if (new_gizmo.get_state() != GLGizmoBase::On)
+        return false; // gizmo refused to be turned on.
 
-    if (new_gizmo)
-        new_gizmo->set_state(GLGizmoBase::On);
     return true;
 }
 
