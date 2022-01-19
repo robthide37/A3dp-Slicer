46c8f82f24127dc992e904b9b3f1c75a719f0491
tamasmeszaros <meszaros.q@gmail.com>
2022-01-19 13:24:37 +0100

    Revert to GLEW 2.1 as most Linux distros as using that

--- deps/GLEW/GLEW.cmake
+++ deps/GLEW/GLEW.cmake
@@ -4,8 +4,8 @@ find_package(OpenGL QUIET REQUIRED)
 
 prusaslicer_add_cmake_project(
   GLEW
-  URL https://sourceforge.net/projects/glew/files/glew/2.2.0/glew-2.2.0.zip
-  URL_HASH SHA256=a9046a913774395a095edcc0b0ac2d81c3aacca61787b39839b941e9be14e0d4
+  URL https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.zip
+  URL_HASH SHA256=2700383d4de2455f06114fbaf872684f15529d4bdc5cdea69b5fb0e9aa7763f1
   SOURCE_SUBDIR build/cmake
   CMAKE_ARGS
     -DBUILD_UTILS=OFF
