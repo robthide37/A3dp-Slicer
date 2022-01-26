dee0c3e33fd4021376425a081f220953abd97e8c
tamasmeszaros <meszaros.q@gmail.com>
2022-01-26 13:09:40 +0100

    Fix OpenVDB on Windows

--- deps/OpenVDB/OpenVDB.cmake
+++ deps/OpenVDB/OpenVDB.cmake
@@ -8,8 +8,8 @@ endif()
 
 prusaslicer_add_cmake_project(OpenVDB
     # 8.2 patched
-    URL https://github.com/tamasmeszaros/openvdb/archive/d49d53332d054e71dfa444a8cb5f965212a57a92.zip
-    URL_HASH SHA256=2a5a50f3565a53ab7e7b78d6e2b345faa1f3b49891d004a3f052cb8cf01a5dfa
+    URL https://github.com/tamasmeszaros/openvdb/archive/a68fd58d0e2b85f01adeb8b13d7555183ab10aa5.zip
+    URL_HASH SHA256=f353e7b99bd0cbfc27ac9082de51acf32a8bc0b3e21ff9661ecca6f205ec1d81
     DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
     CMAKE_ARGS
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
