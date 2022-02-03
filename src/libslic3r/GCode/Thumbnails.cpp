030f4601149704fb3213ef44ca9c9910a1548ed0
enricoturri1966 <enricoturri@seznam.cz>
2022-02-03 13:24:30 +0100

    Follow-up of 87cff55856ae14096cd57f8e2542f1f1caef7167 - Implementation of compress_thumbnail_jpg()

--- src/libslic3r/GCode/Thumbnails.cpp
+++ src/libslic3r/GCode/Thumbnails.cpp
@@ -2,6 +2,7 @@
 #include "../miniz_extension.hpp"
 
 #include <qoi/qoi.h>
+#include <jpeg-compressor/jpge.h>
 
 namespace Slic3r::GCodeThumbnails {
 
@@ -11,6 +12,12 @@ struct CompressedPNG : CompressedImageBuffer
 {
     ~CompressedPNG() override { if (data) mz_free(data); }
     std::string_view tag() const override { return "thumbnail"sv; }
+};
+
+struct CompressedJPG : CompressedImageBuffer
+{
+    ~CompressedJPG() override { free(data); }
+    std::string_view tag() const override { return "thumbnail_JPG"sv; }
 };
 
 struct CompressedQOI : CompressedImageBuffer 
@@ -28,9 +35,28 @@ std::unique_ptr<CompressedImageBuffer> compress_thumbnail_png(const ThumbnailDat
 
 std::unique_ptr<CompressedImageBuffer> compress_thumbnail_jpg(const ThumbnailData& data)
 {
-    //FIXME change to JPG
-    auto out = std::make_unique<CompressedPNG>();
-    out->data = tdefl_write_image_to_png_file_in_memory_ex((const void*)data.pixels.data(), data.width, data.height, 4, &out->size, MZ_DEFAULT_LEVEL, 1);
+    // Take vector of RGBA pixels and flip the image vertically
+    std::vector<uint8_t> rgba_pixels(data.pixels.size());
+    const size_t row_size = data.width * 4;
+    for (size_t y = 0; y < data.height; ++y)
+        ::memcpy(rgba_pixels.data() + (data.height - y - 1) * row_size, data.pixels.data() + y * row_size, row_size);
+
+    auto out = std::make_unique<CompressedJPG>();
+
+    std::vector<jpge::uint8> compressed_data(data.pixels.size());
+    jpge::params params;
+    params.m_quality = 85;
+    params.m_subsampling = jpge::H2V2;
+    params.m_no_chroma_discrim_flag = false;
+    params.m_two_pass_flag = false;
+    params.m_use_std_tables = false;
+
+    int compressed_data_size = int(compressed_data.size());
+    if (jpge::compress_image_to_jpeg_file_in_memory(compressed_data.data(), compressed_data_size, data.width, data.height, 4, rgba_pixels.data(), params)) {
+        out->data = malloc(compressed_data_size);
+        out->size = size_t(compressed_data_size);
+        ::memcpy(out->data, (const void*)compressed_data.data(), out->size);
+    }
     return out;
 }
 
