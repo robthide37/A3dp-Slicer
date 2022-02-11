5ac0e124e4fb5b40c3f4ce3706c5cccb408d69b7
YuSanka <yusanka@gmail.com>
2022-02-11 11:17:12 +0100

    Fix for #6364 - MacOS / Minimize window / Shortcut CMD + M
    There was a bug in wxWidgets 3.1.4. (https://github.com/wxWidgets/wxWidgets/issues/18998)
    So, we cherry-picked fixing commit from the upstream wxWidgets to our "v3.1.4-patched"

--- deps/wxWidgets/wxWidgets.cmake
+++ deps/wxWidgets/wxWidgets.cmake
@@ -13,7 +13,7 @@ prusaslicer_add_cmake_project(wxWidgets
     # GIT_REPOSITORY "https://github.com/prusa3d/wxWidgets"
     # GIT_TAG tm_cross_compile #${_wx_git_tag}
     URL https://github.com/prusa3d/wxWidgets/archive/refs/heads/v3.1.4-patched.zip
-    URL_HASH SHA256=69dec874981d2fc3d90345660c27f3450d8430c483e8446edcc87b6ed18bff8f
+    URL_HASH SHA256=e51493d071ca56f383955a86144b63b3c23c24565e235848dbe0f99c5f5fd57c
     DEPENDS ${PNG_PKG} ${ZLIB_PKG} ${EXPAT_PKG} dep_TIFF dep_JPEG
     CMAKE_ARGS
         -DwxBUILD_PRECOMP=ON
