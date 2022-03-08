3e47253725bb8293c06f23733a61b3580e557199
enricoturri1966 <enricoturri@seznam.cz>
2022-03-08 10:59:12 +0100

    Follow-up of 66fa2b1e2685b9b01232b89488a97d6d413ea240 - Cleanup

--- resources/shaders/mm_contour_attr.fs
+++ resources/shaders/mm_contour_attr.fs
@@ -1,17 +1,12 @@
 #version 110
 
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-uniform vec4 uniform_color;
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 const float EPSILON = 0.0001;
+
+uniform vec4 uniform_color;
 
 void main()
 {
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     gl_FragColor = uniform_color;
-//    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     // Values inside depth buffer for fragments of the contour of a selected area are offset
     // by small epsilon to solve z-fighting between painted triangles and contour lines.
     gl_FragDepth = gl_FragCoord.z - EPSILON;
