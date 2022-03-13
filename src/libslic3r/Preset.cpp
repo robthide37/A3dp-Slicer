457e8bd09e3a7e89f45b3e1ad015e3c9b29dafb3
supermerill <merill@free.fr>
2022-03-13 19:15:39 +0100

    fix no "changed" flag in Printer Settings /Extruder supermerill/SuperSlicer#2523

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -1485,7 +1485,7 @@ inline t_config_option_keys deep_diff(const ConfigBase &config_this, const Confi
             && (ignore_phony || !(this_opt->is_phony() && other_opt->is_phony()))
             && ((*this_opt != *other_opt) || (this_opt->is_phony() != other_opt->is_phony())))
         {
-            if (opt_key == "bed_shape" || opt_key == "compatible_prints" || opt_key == "compatible_printers" || "filament_ramming_parameters" || "gcode_substitutions") {
+            if (opt_key == "bed_shape" || opt_key == "compatible_prints" || opt_key == "compatible_printers" || opt_key == "filament_ramming_parameters" || opt_key == "gcode_substitutions") {
                 // Scalar variable, or a vector variable, which is independent from number of extruders,
                 // thus the vector is presented to the user as a single input.
                 // Merill: these are 'button' special settings.
