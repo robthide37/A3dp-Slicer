5b9f3ccf722f5af5c634bc2adcc24b18499a86fe
remi <remi-j.durand@thalesgroup.com>
2022-03-28 16:00:43 +0200

    fix compile on old compiler

--- src/slic3r/GUI/Gizmos/GLGizmoSeam.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoSeam.cpp
@@ -99,7 +99,7 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
 
     float caption_max = 0.f;
     float total_text_max = 0.f;
-    for (const auto &t : std::array<std::string, 3>{"enforce", "block", "remove"}) {
+    for (const std::string& t : {"enforce", "block", "remove"}) {
         caption_max    = std::max(caption_max, m_imgui->calc_text_size(m_desc[t + "_caption"]).x);
         total_text_max = std::max(total_text_max, m_imgui->calc_text_size(m_desc[t]).x);
     }
@@ -120,7 +120,7 @@ void GLGizmoSeam::on_render_input_window(float x, float y, float bottom_limit)
         m_imgui->text(text);
     };
 
-    for (const auto &t : std::array<std::string, 3>{"enforce", "block", "remove"})
+    for(const std::string& t : {"enforce", "block", "remove"})
         draw_text_with_caption(m_desc.at(t + "_caption"), m_desc.at(t));
 
     ImGui::Separator();
