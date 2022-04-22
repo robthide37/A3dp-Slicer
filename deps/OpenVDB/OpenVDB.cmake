0ef4bfd5cb2a786497eda9af15a9f46fa0257cdf
supermerill <merill@free.fr>
2022-04-22 15:13:28 +0200

    macos build without zstd.

--- deps/OpenVDB/OpenVDB.cmake
+++ deps/OpenVDB/OpenVDB.cmake
@@ -6,44 +6,29 @@ else()
     set(_build_static ON)
 endif()
 
-if (APPLE)
-    find_package(zstd REQUIRED)
-endif()
+set (_openvdb_vdbprint ON)
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
+    # Build fails on raspberry pi due to missing link directive to latomic
+    # Let's hope it will be fixed soon.
+    set (_openvdb_vdbprint OFF)
+endif ()
 
-if(APPLE AND NOT BUILD_SHARED_LIBS)
-    prusaslicer_add_cmake_project(OpenVDB
-        URL https://github.com/tamasmeszaros/openvdb/archive/refs/tags/v6.2.1-prusa3d.zip #v6.2.1 patched
-        URL_HASH SHA256=caf9f0c91976722883ff9cb32420ef142af22f7e625fc643b91c23d6e4172f62 
-        DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
-        CMAKE_ARGS
-            -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
-            -DOPENVDB_BUILD_PYTHON_MODULE=OFF
-            -DUSE_BLOSC=ON
-            -DOPENVDB_CORE_SHARED=${_build_shared} 
-            -DOPENVDB_CORE_STATIC=${_build_static}
-            -DOPENVDB_ENABLE_RPATH:BOOL=OFF
-            -DTBB_STATIC=${_build_static}
-            -DOPENVDB_BUILD_VDB_PRINT=ON
-            -DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
-#            -DCMAKE_STATIC_LINKER_FLAGS="${BREW_ZSTD_PREFIX}/lib/libzstd.a"
-    )
-else()
-    prusaslicer_add_cmake_project(OpenVDB
-        URL https://github.com/tamasmeszaros/openvdb/archive/refs/tags/v6.2.1-prusa3d.zip #v6.2.1 patched
-        URL_HASH SHA256=caf9f0c91976722883ff9cb32420ef142af22f7e625fc643b91c23d6e4172f62 
-        DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
-        CMAKE_ARGS
-            -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
-            -DOPENVDB_BUILD_PYTHON_MODULE=OFF
-            -DUSE_BLOSC=ON
-            -DOPENVDB_CORE_SHARED=${_build_shared} 
-            -DOPENVDB_CORE_STATIC=${_build_static}
-            -DOPENVDB_ENABLE_RPATH:BOOL=OFF
-            -DTBB_STATIC=${_build_static}
-            -DOPENVDB_BUILD_VDB_PRINT=ON
-            -DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
-    )
-endif()
+prusaslicer_add_cmake_project(OpenVDB
+    # 8.2 patched
+    URL https://github.com/tamasmeszaros/openvdb/archive/a68fd58d0e2b85f01adeb8b13d7555183ab10aa5.zip
+    URL_HASH SHA256=f353e7b99bd0cbfc27ac9082de51acf32a8bc0b3e21ff9661ecca6f205ec1d81
+    DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
+    CMAKE_ARGS
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
+        -DOPENVDB_BUILD_PYTHON_MODULE=OFF
+        -DUSE_BLOSC=ON
+        -DOPENVDB_CORE_SHARED=${_build_shared} 
+        -DOPENVDB_CORE_STATIC=${_build_static}
+        -DOPENVDB_ENABLE_RPATH:BOOL=OFF
+        -DTBB_STATIC=${_build_static}
+        -DOPENVDB_BUILD_VDB_PRINT=${_openvdb_vdbprint}
+        -DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
+)
 
 if (MSVC)
     if (${DEP_DEBUG})
