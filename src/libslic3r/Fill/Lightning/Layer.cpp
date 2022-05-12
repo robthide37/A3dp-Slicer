bff32c9cdbcf30603d392777f02365d1bf009a15
Lukáš Hejl <hejl.lukas@gmail.com>
2022-05-12 09:49:07 +0200

    Added cancellation to Lightning infill.

--- src/libslic3r/Fill/Lightning/Layer.cpp
+++ src/libslic3r/Fill/Lightning/Layer.cpp
@@ -44,10 +44,12 @@ void Layer::generateNewTrees
     const BoundingBox& current_outlines_bbox,
     const EdgeGrid::Grid& outlines_locator,
     const coord_t supporting_radius,
-    const coord_t wall_supporting_radius
+    const coord_t wall_supporting_radius,
+    const std::function<void()> &throw_on_cancel_callback
 )
 {
     DistanceField distance_field(supporting_radius, current_outlines, current_outlines_bbox, current_overhang);
+    throw_on_cancel_callback();
 
     SparseNodeGrid tree_node_locator;
     fillLocator(tree_node_locator, current_outlines_bbox);
@@ -56,6 +58,7 @@ void Layer::generateNewTrees
     // Determine next point from tree/outline areas via distance-field
     Point unsupported_location;
     while (distance_field.tryGetNextPoint(&unsupported_location)) {
+        throw_on_cancel_callback();
         GroundingLocation grounding_loc = getBestGroundingLocation(
             unsupported_location, current_outlines, current_outlines_bbox, outlines_locator, supporting_radius, wall_supporting_radius, tree_node_locator);
 
