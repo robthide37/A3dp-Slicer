b42e4b29272762b925e78c19fb5b0fd834eecf61
enricoturri1966 <enricoturri@seznam.cz>
2022-06-08 13:12:29 +0200

    GLTexture - Use BitmapCache::nsvgParseFromFileWithReplace() in place of nsvgParseFromFile() to load images from files

--- src/slic3r/GUI/GLTexture.cpp
+++ src/slic3r/GUI/GLTexture.cpp
@@ -7,6 +7,7 @@
 #include "GUI_App.hpp"
 #include "GLModel.hpp"
 #endif // ENABLE_LEGACY_OPENGL_REMOVAL
+#include "BitmapCache.hpp"
 
 #include <GL/glew.h>
 
@@ -204,7 +205,7 @@ bool GLTexture::load_from_svg_files_as_sprites_array(const std::vector<std::stri
         if (!boost::algorithm::iends_with(filename, ".svg"))
             continue;
 
-        NSVGimage* image = nsvgParseFromFile(filename.c_str(), "px", 96.0f);
+        NSVGimage* image = BitmapCache::nsvgParseFromFileWithReplace(filename.c_str(), "px", 96.0f, {});
         if (image == nullptr)
             continue;
 
@@ -536,7 +537,7 @@ bool GLTexture::load_from_svg(const std::string& filename, bool use_mipmaps, boo
 {
     bool compression_enabled = compress && GLEW_EXT_texture_compression_s3tc;
 
-    NSVGimage* image = nsvgParseFromFile(filename.c_str(), "px", 96.0f);
+    NSVGimage* image = BitmapCache::nsvgParseFromFileWithReplace(filename.c_str(), "px", 96.0f, {});
     if (image == nullptr) {
         reset();
         return false;
