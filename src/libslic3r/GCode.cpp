a91ef83f9ec592f82c125f890978c004dd59b2c6
supermerill <merill@free.fr>
2022-07-12 15:39:42 +0200

    Fix crashing gcode when seam_gap is null and wipe inside is activated supermerill/SuperSlicer#2850

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -3415,7 +3415,7 @@ std::string GCode::extrude_loop_vase(const ExtrusionLoop &original_loop, const s
     ExtrusionPaths &paths = loop_to_seam.paths;
     if (false && m_enable_loop_clipping && m_writer.tool_is_extruder()) {
         coordf_t clip_length = scale_(m_config.seam_gap.get_abs_value(m_writer.tool()->id(), EXTRUDER_CONFIG_WITH_DEFAULT(nozzle_diameter, 0)));
-    coordf_t min_clip_length = scale_(EXTRUDER_CONFIG_WITH_DEFAULT(nozzle_diameter, 0)) * 0.15;
+        coordf_t min_clip_length = scale_(EXTRUDER_CONFIG_WITH_DEFAULT(nozzle_diameter, 0)) * 0.15;
 
         // get paths
         ExtrusionPaths clipped;
@@ -3848,15 +3848,19 @@ std::string GCode::extrude_loop(const ExtrusionLoop &original_loop, const std::s
 
     std::string gcode;
 
-    // generate the unretracting/wipe start move (same thign than for the end, but on the other side)
+    // generate the unretracting/wipe start move (same thing than for the end, but on the other side)
     assert(!paths.empty() && paths.front().size() > 1 && !paths.back().empty());
-    if (EXTRUDER_CONFIG_WITH_DEFAULT(wipe_inside_start, true) && !paths.empty() && paths.front().size() > 1 && !paths.back().empty() && paths.front().role() == erExternalPerimeter) {
+    if (EXTRUDER_CONFIG_WITH_DEFAULT(wipe_inside_start, true) && !paths.empty() && paths.front().size() > 1 && paths.back().size() > 1 && paths.front().role() == erExternalPerimeter) {
         //note: previous & next are inverted to extrude "in the opposite direction, ans we are "rewinding"
         //Point previous_point = paths.back().polyline.points.back();
         Point previous_point = paths.front().polyline.points[1];
         Point current_point = paths.front().polyline.points.front();
         //Point next_point = paths.front().polyline.points[1];
         Point next_point = paths.back().polyline.points.back();
+        if (next_point == current_point) {
+            //can happen if seam_gap is null
+            next_point = paths.back().polyline.points[paths.back().polyline.points.size()-2];
+        }
         Point a = next_point;  // second point
         Point b = previous_point;  // second to last point
         if (is_hole_loop ? (!is_full_loop_ccw) : (is_full_loop_ccw)) {
