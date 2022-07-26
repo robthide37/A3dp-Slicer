77f5973c25ef0edc6db95de13c5976722b57a023
Lukáš Hejl <hejl.lukas@gmail.com>
2022-07-26 13:42:05 +0200

    Fix of #8446: Non-planar Voronoi diagram.
    This is the follow-up to 63c66f4f189b6f3d3a3b95ddd158c66ee5166420. Detection of non-planar (degenerated) Voronoi diagrams was rewritten to check if all neighboring edges of the Voronoi vertex are CCW ordered.

--- src/libslic3r/Arachne/SkeletalTrapezoidation.cpp
+++ src/libslic3r/Arachne/SkeletalTrapezoidation.cpp
@@ -17,11 +17,8 @@
 #include "Utils.hpp"
 #include "SVG.hpp"
 #include "Geometry/VoronoiVisualUtils.hpp"
+#include "Geometry/VoronoiUtilsCgal.hpp"
 #include "../EdgeGrid.hpp"
-
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Arr_segment_traits_2.h>
-#include <CGAL/Surface_sweep_2_algorithms.h>
 
 #define SKELETAL_TRAPEZOIDATION_BEAD_SEARCH_MAX 1000 //A limit to how long it'll keep searching for adjacent beads. Increasing will re-use beadings more often (saving performance), but search longer for beading (costing performance).
 
@@ -391,40 +388,6 @@ SkeletalTrapezoidation::SkeletalTrapezoidation(const Polygons& polys, const Bead
     constructFromPolygons(polys);
 }
 
-using CGAL_Point   = CGAL::Exact_predicates_exact_constructions_kernel::Point_2;
-using CGAL_Segment = CGAL::Arr_segment_traits_2<CGAL::Exact_predicates_exact_constructions_kernel>::Curve_2;
-
-inline static CGAL_Point to_cgal_point(const Voronoi::VD::vertex_type &pt)
-{
-    return {pt.x(), pt.y()};
-}
-
-bool is_voronoi_diagram_planar(const Geometry::VoronoiDiagram &voronoi_diagram) {
-    assert(std::all_of(voronoi_diagram.edges().cbegin(), voronoi_diagram.edges().cend(),
-                       [](const Geometry::VoronoiDiagram::edge_type &edge) { return edge.color() == 0; }));
-
-    std::vector<CGAL_Segment> segments;
-    segments.reserve(voronoi_diagram.num_edges());
-
-    for (const Geometry::VoronoiDiagram::edge_type &edge : voronoi_diagram.edges()) {
-        if (edge.color() != 0)
-            continue;
-
-        if (edge.is_finite() && edge.is_linear()) {
-            segments.emplace_back(to_cgal_point(*edge.vertex0()), to_cgal_point(*edge.vertex1()));
-            edge.color(1);
-            assert(edge.twin() != nullptr);
-            edge.twin()->color(1);
-        }
-    }
-
-    for (const Geometry::VoronoiDiagram::edge_type &edge : voronoi_diagram.edges())
-        edge.color(0);
-
-    std::vector<CGAL_Point> intersections_pt;
-    CGAL::compute_intersection_points(segments.begin(), segments.end(), std::back_inserter(intersections_pt));
-    return intersections_pt.empty();
-}
 
 static bool detect_missing_voronoi_vertex(const Geometry::VoronoiDiagram &voronoi_diagram, const std::vector<SkeletalTrapezoidation::Segment> &segments) {
     for (VoronoiUtils::vd_t::cell_type cell : voronoi_diagram.cells()) {
@@ -506,7 +469,7 @@ inline static std::unordered_map<Point, Point, PointHash> try_to_fix_degenerated
     voronoi_diagram.clear();
     construct_voronoi(segments.begin(), segments.end(), &voronoi_diagram);
 
-    assert(is_voronoi_diagram_planar(voronoi_diagram));
+    assert(Geometry::VoronoiUtilsCgal::is_voronoi_diagram_planar_intersection(voronoi_diagram));
 
     return vertex_mapping;
 }
@@ -562,26 +525,38 @@ void SkeletalTrapezoidation::constructFromPolygons(const Polygons& polys)
 #endif
 
 #ifdef ARACHNE_DEBUG
-    assert(is_voronoi_diagram_planar(voronoi_diagram));
+    assert(is_voronoi_diagram_planar_intersection(voronoi_diagram));
 #endif
 
-    // Try to detect cases when some Voronoi vertex is missing.
-    // When any Voronoi vertex is missing, rotate input polygon and try again.
-    const bool                                  has_missing_voronoi_vertex = detect_missing_voronoi_vertex(voronoi_diagram, segments);
-    const double                                fix_angle                  = PI / 6;
+    // Try to detect cases when some Voronoi vertex is missing and when
+    // the Voronoi diagram is not planar.
+    // When any Voronoi vertex is missing, or the Voronoi diagram is not
+    // planar, rotate the input polygon and try again.
+    const bool   has_missing_voronoi_vertex = detect_missing_voronoi_vertex(voronoi_diagram, segments);
+    // Detection of non-planar Voronoi diagram detects at least GH issues #8474, #8514 and #8446.
+    const bool   is_voronoi_diagram_planar  = Geometry::VoronoiUtilsCgal::is_voronoi_diagram_planar_angle(voronoi_diagram);
+    const double fix_angle                  = PI / 6;
+
     std::unordered_map<Point, Point, PointHash> vertex_mapping;
     // polys_copy is referenced through items stored in the std::vector segments.
     Polygons                                    polys_copy = polys;
-    if (has_missing_voronoi_vertex) {
-        BOOST_LOG_TRIVIAL(warning) << "Detected missing Voronoi vertex, input polygons will be rotated back and forth.";
+    if (has_missing_voronoi_vertex || !is_voronoi_diagram_planar) {
+        if (has_missing_voronoi_vertex)
+            BOOST_LOG_TRIVIAL(warning) << "Detected missing Voronoi vertex, input polygons will be rotated back and forth.";
+        else if (!is_voronoi_diagram_planar)
+            BOOST_LOG_TRIVIAL(warning) << "Detected non-planar Voronoi diagram, input polygons will be rotated back and forth.";
+
         vertex_mapping = try_to_fix_degenerated_voronoi_diagram_by_rotation(voronoi_diagram, polys, polys_copy, segments, fix_angle);
 
         assert(!detect_missing_voronoi_vertex(voronoi_diagram, segments));
+        assert(Geometry::VoronoiUtilsCgal::is_voronoi_diagram_planar_angle(voronoi_diagram));
         if (detect_missing_voronoi_vertex(voronoi_diagram, segments))
             BOOST_LOG_TRIVIAL(error) << "Detected missing Voronoi vertex even after the rotation of input.";
+        else if (!Geometry::VoronoiUtilsCgal::is_voronoi_diagram_planar_angle(voronoi_diagram))
+            BOOST_LOG_TRIVIAL(error) << "Detected non-planar Voronoi diagram even after the rotation of input.";
     }
 
-    bool degenerated_voronoi_diagram = has_missing_voronoi_vertex;
+    bool degenerated_voronoi_diagram = has_missing_voronoi_vertex || !is_voronoi_diagram_planar;
 
 process_voronoi_diagram:
     assert(this->graph.edges.empty() && this->graph.nodes.empty() && this->vd_edge_to_he_edge.empty() && this->vd_node_to_he_node.empty());
@@ -644,8 +619,6 @@ process_voronoi_diagram:
     // When this degenerated Voronoi diagram is processed, the resulting half-edge structure contains some edges that don't have
     // a twin edge. Based on this, we created a fast mechanism that detects those causes and tries to recompute the Voronoi
     // diagram on slightly rotated input polygons that usually make the Voronoi generator generate a non-degenerated Voronoi diagram.
-    // FIXME Lukas H.: Replace has_missing_twin_edge with detection if the Voronoi diagram is planar using a custom algorithm based on the
-    //                 sweeping line algorithm. At least it is required to fix GH #8446.
     if (!degenerated_voronoi_diagram && has_missing_twin_edge(this->graph)) {
         BOOST_LOG_TRIVIAL(warning) << "Detected degenerated Voronoi diagram, input polygons will be rotated back and forth.";
         degenerated_voronoi_diagram = true;
@@ -655,7 +628,7 @@ process_voronoi_diagram:
         if (detect_missing_voronoi_vertex(voronoi_diagram, segments))
             BOOST_LOG_TRIVIAL(error) << "Detected missing Voronoi vertex after the rotation of input.";
 
-        assert(is_voronoi_diagram_planar(voronoi_diagram));
+        assert(Geometry::VoronoiUtilsCgal::is_voronoi_diagram_planar_intersection(voronoi_diagram));
 
         this->graph.edges.clear();
         this->graph.nodes.clear();
