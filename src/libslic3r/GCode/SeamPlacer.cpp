3f361871016c8f3106326cd16d91767f1d876a61
PavelMikus <pavel.mikus.mail@seznam.cz>
2022-08-03 15:34:41 +0200

    increase segments count for smooth long strings

--- src/libslic3r/GCode/SeamPlacer.cpp
+++ src/libslic3r/GCode/SeamPlacer.cpp
@@ -1393,7 +1393,9 @@ void SeamPlacer::align_seam_points(const PrintObject *po, const SeamPlacerImpl::
             }
 
             // Curve Fitting
-            size_t number_of_segments = std::max(size_t(1), size_t(segments_count / SeamPlacer::seam_align_seams_per_segment));
+            size_t number_of_segments = std::max(
+                    std::max(size_t(1), seam_string.size() / 80),
+                    size_t(segments_count / SeamPlacer::seam_align_seams_per_segment));
             auto curve = Geometry::fit_cubic_bspline(observations, observation_points, weights, number_of_segments);
 
             // Do alignment - compute fitted point for each point in the string from its Z coord, and store the position into
