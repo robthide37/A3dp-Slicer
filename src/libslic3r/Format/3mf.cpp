robthide37/A3dp-Slicer

commit 7912613dc8e11db2ddea8018ce16b26a01756a3b

Cut WIP: Fixed crash on second "Perform cut" + some code cleaning

src/slic3r/GUI/Gizmos/GLGizmoCut.cpp
@@ -163,8 +163,6 @@ static void init_from_angle_arc(GLModel& model, double angle, double radius)
 
 //! --
 
-#define use_grabber_extension 1
-
 GLGizmoCut3D::GLGizmoCut3D(GLCanvas3D& parent, const std::string& icon_filename, unsigned int sprite_id)
     : GLGizmoBase(parent, icon_filename, sprite_id)
     , m_connector_type (CutConnectorType::Plug)
@@ -1030,12 +1028,9 @@ void GLGizmoCut3D::on_dragging(const UpdateData& data)
     CutConnectors& connectors = m_c->selection_info()->model_object()->cut_connectors;
 
     if (m_hover_id == Z) {
-#if use_grabber_extension
         Vec3d starting_box_center = m_plane_center - Vec3d::UnitZ();// some Margin
         rotate_vec3d_around_center(starting_box_center, Geometry::Transformation(m_rotation_m).get_rotation(), m_plane_center);
-#else
-        const Vec3d& starting_box_center = m_plane_center;
-#endif
+
         const Vec3d& starting_drag_position = m_plane_center;
         double projection = 0.0;
 
@@ -1184,14 +1179,10 @@ BoundingBoxf3 GLGizmoCut3D::transformed_bounding_box(bool revert_move /*= false*
     Vec3d cut_center_offset = m_plane_center - instance_offset;
     cut_center_offset[Z] -= m_c->selection_info()->get_sla_shift();
 
-    const Vec3d rotation = Geometry::Transformation(m_rotation_m).get_rotation();
     const auto move  = Geometry::assemble_transform(-cut_center_offset);
-    const auto rot_z = Geometry::assemble_transform(Vec3d::Zero(), Vec3d(0, 0, -rotation.z()));
-    const auto rot_y = Geometry::assemble_transform(Vec3d::Zero(), Vec3d(0, -rotation.y(), 0));
-    const auto rot_x = Geometry::assemble_transform(Vec3d::Zero(), Vec3d(-rotation.x(), 0, 0));
-    const auto move2  = Geometry::assemble_transform(m_plane_center);
+    const auto move2 = Geometry::assemble_transform(m_plane_center);
 
-    const auto cut_matrix = (revert_move ? move2 : Transform3d::Identity()) * rot_x * rot_y * rot_z * move;
+    const auto cut_matrix = (revert_move ? move2 : Transform3d::Identity()) * m_rotation_m.inverse() * move;
 
     const Selection& selection = m_parent.get_selection();
     const Selection::IndicesList& idxs = selection.get_volume_idxs();
@@ -1207,7 +1198,10 @@ BoundingBoxf3 GLGizmoCut3D::transformed_bounding_box(bool revert_move /*= false*
                 volume->get_instance_mirror()
             );
 
-            ret.merge(volume->transformed_convex_hull_bounding_box(cut_matrix * instance_matrix * volume->get_volume_transformation().get_matrix()));
+            auto volume_travo = instance_matrix * volume->get_volume_transformation().get_matrix();
+            auto volume_offset = Geometry::Transformation(volume_travo).get_offset();
+
+            ret.merge(volume->transformed_convex_hull_bounding_box(cut_matrix * volume_travo));
         }
     }
     return ret;
@@ -1223,7 +1217,7 @@ bool GLGizmoCut3D::update_bb()
         set_center_pos(m_bb_center + m_center_offset, true);
 
         m_radius = box.radius();
-        m_grabber_connection_len = std::min<double>(0.75 * m_radius, 35.0);
+        m_grabber_connection_len = 0.75 * m_radius;// std::min<double>(0.75 * m_radius, 35.0);
         m_grabber_radius = m_grabber_connection_len * 0.85;
 
         m_snap_coarse_in_radius   = m_grabber_radius / 3.0f;
@@ -1265,8 +1259,11 @@ void GLGizmoCut3D::init_picking_models()
 
 void GLGizmoCut3D::on_render()
 {
-    if (update_bb())
+    if (update_bb() || force_update_clipper_on_render) {
         update_clipper_on_render();
+        if (force_update_clipper_on_render)
+            m_c->object_clipper()->set_behavior(m_connectors_editing, m_connectors_editing, 0.4f);
+    }
 
     init_picking_models();
 
@@ -1284,11 +1281,6 @@ void GLGizmoCut3D::on_render()
     }
     if (!m_angle_arc.is_initialized() || m_angle != 0.0)
         init_from_angle_arc(m_angle_arc, m_angle, m_grabber_connection_len);
-
-    if (force_update_clipper_on_render) {
-        update_clipper_on_render();
-        m_c->object_clipper()->set_behavior(m_connectors_editing, m_connectors_editing, 0.4f);
-    }
 
     render_connectors();
 
@@ -1725,6 +1717,8 @@ void GLGizmoCut3D::perform_cut(const Selection& selection)
         {
             // update connectors pos as offset of its center before cut performing
             if (has_connectors && m_connector_mode == CutConnectorMode::Manual) {
+                m_selected.clear();
+
                 Plater::TakeSnapshot snapshot(plater, _L("Cut by Plane"));
                 for (CutConnector& connector : mo->cut_connectors) {
                     connector.rotation = rotation;
@@ -1736,13 +1730,9 @@ void GLGizmoCut3D::perform_cut(const Selection& selection)
                     }
                     else {
                         // culculate shift of the connector center regarding to the position on the cut plane
-#if use_grabber_extension
                         Vec3d shifted_center = m_plane_center + Vec3d::UnitZ();
                         rotate_vec3d_around_center(shifted_center, rotation, m_plane_center);
                         Vec3d norm = (shifted_center - m_plane_center).normalized();
-#else
-                        Vec3d norm = (m_grabbers[0].center - m_plane_center).normalize();
-#endif
                         connector.pos += norm * (0.5 * connector.height);
                     }
                 }
@@ -1758,7 +1748,6 @@ void GLGizmoCut3D::perform_cut(const Selection& selection)
             only_if(m_rotate_upper, ModelObjectCutAttribute::FlipUpper) | 
             only_if(m_rotate_lower, ModelObjectCutAttribute::FlipLower) | 
             only_if(create_dowels_as_separate_object, ModelObjectCutAttribute::CreateDowels));
-        m_selected.clear();
     }
     else {
         // the object is SLA-elevated and the plane is under it.
@@ -1871,7 +1860,6 @@ bool GLGizmoCut3D::process_cut_line(SLAGizmoEventType action, const Vec2d& mouse
         m_line_beg = pt;
         m_line_end = pt;
         return true;
-//        volumes_trafos[i] = model_object->volumes[i]->get_matrix();
     }
 
     if (cut_line_processing()) {

src/slic3r/GUI/Plater.cpp
@@ -5949,18 +5949,17 @@ void Slic3r::GUI::Plater::cut(size_t obj_idx, size_t instance_idx, const Vec3d& 
     // suppress to call selection update for Object List to avoid call of early Gizmos on/off update
     p->load_model_objects(new_objects, false, false);
 
-    Selection& selection = p->get_selection();
-    size_t last_id = p->model.objects.size() - 1;
-    for (size_t i = 0; i < new_objects.size(); ++i)
-        selection.add_object((unsigned int)(last_id - i), i == 0);
-    this->allow_snapshots();
-
     // now process all updates of the 3d scene
     update();
     // Update InfoItems in ObjectList after update() to use of a correct value of the GLCanvas3D::is_sinking(),
     // which is updated after a view3D->reload_scene(false, flags & (unsigned int)UpdateParams::FORCE_FULL_SCREEN_REFRESH) call
     for (size_t idx = 0; idx < p->model.objects.size(); idx++)
         wxGetApp().obj_list()->update_info_items(idx);
+
+    Selection& selection = p->get_selection();
+    size_t last_id = p->model.objects.size() - 1;
+    for (size_t i = 0; i < new_objects.size(); ++i)
+        selection.add_object((unsigned int)(last_id - i), i == 0);
 }
 
 void Plater::export_gcode(bool prefer_removable)

src/libslic3r/Model.cpp
@@ -1545,7 +1545,7 @@ ModelObjectPtrs ModelObject::cut(size_t instance, const Vec3d& cut_center, const
                         // for lower part change type of connector from NEGATIVE_VOLUME to MODEL_PART if this connector is a plug
                         vol->set_type(ModelVolumeType::MODEL_PART);
                 }
-                if (volume->cut_info.connector_type == CutConnectorType::Dowel) {
+                if (dowels && volume->cut_info.connector_type == CutConnectorType::Dowel) {
                     // add one more solid part same as connector if this connector is a dowel
                     ModelVolume* vol = dowels->add_volume(*volume);
                     vol->set_type(ModelVolumeType::MODEL_PART);

src/slic3r/GUI/GLCanvas3D.cpp
@@ -3469,6 +3469,7 @@ void GLCanvas3D::on_mouse(wxMouseEvent& evt)
                 if (m_gizmos.get_current_type() != GLGizmosManager::SlaSupports &&
                     m_gizmos.get_current_type() != GLGizmosManager::FdmSupports &&
                     m_gizmos.get_current_type() != GLGizmosManager::Seam &&
+                    m_gizmos.get_current_type() != GLGizmosManager::Cut &&
                     m_gizmos.get_current_type() != GLGizmosManager::MmuSegmentation) {
                     m_rectangle_selection.start_dragging(m_mouse.position, evt.ShiftDown() ? GLSelectionRectangle::EState::Select : GLSelectionRectangle::EState::Deselect);
                     m_dirty = true;

commit cf144da4fe8dc0797096b4c352db2bde7cc0c96f

Cut WIP: Import/Export cut information to/from .3mf file + Fixed a crash during change object selection, when CutGizmo is On + Fixed Undo/Redo (was accidentally broken with 7912613dc8e11db2ddea8018ce16b26a01756a3b)

src/libslic3r/Format/3mf.cpp
@@ -77,6 +77,7 @@ const std::string LAYER_CONFIG_RANGES_FILE = "Metadata/Prusa_Slicer_layer_config
 const std::string SLA_SUPPORT_POINTS_FILE = "Metadata/Slic3r_PE_sla_support_points.txt";
 const std::string SLA_DRAIN_HOLES_FILE = "Metadata/Slic3r_PE_sla_drain_holes.txt";
 const std::string CUSTOM_GCODE_PER_PRINT_Z_FILE = "Metadata/Prusa_Slicer_custom_gcode_per_print_z.xml";
+const std::string CUT_INFORMATION_FILE = "Metadata/Prusa_Slicer_cut_information.xml";
 
 static constexpr const char* MODEL_TAG = "model";
 static constexpr const char* RESOURCES_TAG = "resources";
@@ -416,6 +417,7 @@ namespace Slic3r {
         typedef std::map<int, Geometry> IdToGeometryMap;
         typedef std::map<int, std::vector<coordf_t>> IdToLayerHeightsProfileMap;
         typedef std::map<int, t_layer_config_ranges> IdToLayerConfigRangesMap;
+        typedef std::map<int, CutObjectBase>         IdToCutObjectIdMap;
         typedef std::map<int, std::vector<sla::SupportPoint>> IdToSlaSupportPointsMap;
         typedef std::map<int, std::vector<sla::DrainHole>> IdToSlaDrainHolesMap;
 
@@ -443,6 +445,7 @@ namespace Slic3r {
         IdToGeometryMap m_geometries;
         CurrentConfig m_curr_config;
         IdToMetadataMap m_objects_metadata;
+        IdToCutObjectIdMap m_cut_object_ids;
         IdToLayerHeightsProfileMap m_layer_heights_profiles;
         IdToLayerConfigRangesMap m_layer_config_ranges;
         IdToSlaSupportPointsMap m_sla_support_points;
@@ -474,6 +477,7 @@ namespace Slic3r {
 
         bool _load_model_from_file(const std::string& filename, Model& model, DynamicPrintConfig& config, ConfigSubstitutionContext& config_substitutions);
         bool _extract_model_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat);
+        void _extract_cut_information_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat, ConfigSubstitutionContext& config_substitutions);
         void _extract_layer_heights_profile_config_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat);
         void _extract_layer_config_ranges_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat, ConfigSubstitutionContext& config_substitutions);
         void _extract_sla_support_points_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat);
@@ -676,6 +680,10 @@ namespace Slic3r {
                     // extract slic3r layer heights profile file
                     _extract_layer_heights_profile_config_from_archive(archive, stat);
                 }
+                else if (boost::algorithm::iequals(name, CUT_INFORMATION_FILE)) {
+                    // extract slic3r layer config ranges file
+                    _extract_cut_information_from_archive(archive, stat, config_substitutions);
+                }
                 else if (boost::algorithm::iequals(name, LAYER_CONFIG_RANGES_FILE)) {
                     // extract slic3r layer config ranges file
                     _extract_layer_config_ranges_from_archive(archive, stat, config_substitutions);
@@ -765,6 +773,11 @@ namespace Slic3r {
                 add_error("Unable to find object geometry");
                 return false;
             }
+
+            // m_cut_object_ids are indexed by a 1 based model object index.
+            IdToCutObjectIdMap::iterator cut_object_id = m_cut_object_ids.find(object.second + 1);
+            if (cut_object_id != m_cut_object_ids.end())
+                model_object->cut_id = std::move(cut_object_id->second);
 
             // m_layer_heights_profiles are indexed by a 1 based model object index.
             IdToLayerHeightsProfileMap::iterator obj_layer_heights_profile = m_layer_heights_profiles.find(object.second + 1);
@@ -942,6 +955,48 @@ namespace Slic3r {
         }
 
         return true;
+    }
+
+    void _3MF_Importer::_extract_cut_information_from_archive(mz_zip_archive& archive, const mz_zip_archive_file_stat& stat, ConfigSubstitutionContext& config_substitutions)
+    {
+        if (stat.m_uncomp_size > 0) {
+            std::string buffer((size_t)stat.m_uncomp_size, 0);
+            mz_bool res = mz_zip_reader_extract_file_to_mem(&archive, stat.m_filename, (void*)buffer.data(), (size_t)stat.m_uncomp_size, 0);
+            if (res == 0) {
+                add_error("Error while reading cut information data to buffer");
+                return;
+            }
+
+            std::istringstream iss(buffer); // wrap returned xml to istringstream
+            pt::ptree objects_tree;
+            pt::read_xml(iss, objects_tree);
+
+            for (const auto& object : objects_tree.get_child("objects")) {
+                pt::ptree object_tree = object.second;
+                int obj_idx = object_tree.get<int>("<xmlattr>.id", -1);
+                if (obj_idx <= 0) {
+                    add_error("Found invalid object id");
+                    continue;
+                }
+
+                IdToCutObjectIdMap::iterator object_item = m_cut_object_ids.find(obj_idx);
+                if (object_item != m_cut_object_ids.end()) {
+                    add_error("Found duplicated cut_object_id");
+                    continue;
+                }
+
+                for (const auto& obj_cut_id : object_tree) {
+                    if (obj_cut_id.first != "cut_id")
+                        continue;
+                    pt::ptree cut_id_tree = obj_cut_id.second;
+                    ObjectID obj_id(cut_id_tree.get<size_t>("<xmlattr>.id"));
+                    CutObjectBase cut_id(ObjectID(cut_id_tree.get<size_t>("<xmlattr>.id")),
+                                         cut_id_tree.get<size_t>("<xmlattr>.check_sum"),
+                                         cut_id_tree.get<size_t>("<xmlattr>.connectors_cnt"));
+                    m_cut_object_ids.insert({ obj_idx, std::move(cut_id) });
+                }
+            }
+        }
     }
 
     void _3MF_Importer::_extract_print_config_from_archive(
@@ -2219,6 +2274,7 @@ namespace Slic3r {
         bool _add_object_to_model_stream(mz_zip_writer_staged_context &context, unsigned int& object_id, ModelObject& object, BuildItemsList& build_items, VolumeToOffsetsMap& volumes_offsets);
         bool _add_mesh_to_object_stream(mz_zip_writer_staged_context &context, ModelObject& object, VolumeToOffsetsMap& volumes_offsets);
         bool _add_build_to_model_stream(std::stringstream& stream, const BuildItemsList& build_items);
+        bool _add_cut_information_file_to_archive(mz_zip_archive& archive, Model& model);
         bool _add_layer_height_profile_file_to_archive(mz_zip_archive& archive, Model& model);
         bool _add_layer_config_ranges_file_to_archive(mz_zip_archive& archive, Model& model);
         bool _add_sla_support_points_file_to_archive(mz_zip_archive& archive, Model& model);
@@ -2276,6 +2332,15 @@ namespace Slic3r {
         // This is the one and only file that contains all the geometry (vertices and triangles) of all ModelVolumes.
         IdToObjectDataMap objects_data;
         if (!_add_model_file_to_archive(filename, archive, model, objects_data)) {
+            close_zip_writer(&archive);
+            boost::filesystem::remove(filename);
+            return false;
+        }
+
+        // Adds file with information for object cut ("Metadata/Slic3r_PE_cut_information.txt").
+        // All information for object cut of all ModelObjects are stored here, indexed by 1 based index of the ModelObject in Model.
+        // The index differes from the index of an object ID of an object instance of a 3MF file!
+        if (!_add_cut_information_file_to_archive(archive, model)) {
             close_zip_writer(&archive);
             boost::filesystem::remove(filename);
             return false;
@@ -2777,6 +2842,51 @@ namespace Slic3r {
         }
 
         stream << " </" << BUILD_TAG << ">\n";
+
+        return true;
+    }
+
+    bool _3MF_Exporter::_add_cut_information_file_to_archive(mz_zip_archive& archive, Model& model)
+    {
+        std::string out = "";
+        pt::ptree tree;
+
+        unsigned int object_cnt = 0;
+        for (const ModelObject* object : model.objects) {
+            object_cnt++;
+            pt::ptree& obj_tree = tree.add("objects.object", "");
+
+            obj_tree.put("<xmlattr>.id", object_cnt);
+
+            // Store info for cut_id
+            pt::ptree& cut_id_tree = obj_tree.add("cut_id", "");
+
+            // store cut_id atributes
+            cut_id_tree.put("<xmlattr>.id",             object->cut_id.id().id);
+            cut_id_tree.put("<xmlattr>.check_sum",      object->cut_id.check_sum());
+            cut_id_tree.put("<xmlattr>.connectors_cnt", object->cut_id.connectors_cnt());
+        }
+
+        if (!tree.empty()) {
+            std::ostringstream oss;
+            pt::write_xml(oss, tree);
+            out = oss.str();
+
+            // Post processing("beautification") of the output string for a better preview
+            boost::replace_all(out, "><object", ">\n <object");
+            boost::replace_all(out, "><cut_id", ">\n  <cut_id");
+            boost::replace_all(out, "></cut_id>", ">\n  </cut_id>");
+            boost::replace_all(out, "></object>", ">\n </object>");
+            // OR just 
+            boost::replace_all(out, "><", ">\n<");
+        }
+
+        if (!out.empty()) {
+            if (!mz_zip_writer_add_mem(&archive, CUT_INFORMATION_FILE.c_str(), (const void*)out.data(), out.length(), MZ_DEFAULT_COMPRESSION)) {
+                add_error("Unable to add cut information file to archive");
+                return false;
+            }
+        }
 
         return true;
     }

src/libslic3r/Model.hpp
@@ -351,7 +351,7 @@ public:
     // Holes to be drilled into the object so resin can flow out
     sla::DrainHoles         sla_drain_holes;
 
-    // Connectors to be added into the object after cut
+    // Connectors to be added into the object before cut and are used to create a solid/negative volumes during a cut perform
     CutConnectors           cut_connectors;
     CutObjectBase           cut_id;
 

src/libslic3r/ObjectID.hpp
@@ -89,7 +89,9 @@ private:
 	friend class cereal::access;
 	friend class Slic3r::UndoRedo::StackImpl;
 	template<class Archive> void serialize(Archive &ar) { ar(m_id); }
+protected: // #vbCHECKME && #ysFIXME
     ObjectBase(const ObjectID id) : m_id(id) {}
+private:
   	template<class Archive> static void load_and_construct(Archive & ar, cereal::construct<ObjectBase> &construct) { ObjectID id; ar(id); construct(id); }
 };
 
@@ -141,6 +143,8 @@ public:
     // Constructor with ignored int parameter to assign an invalid ID, to be replaced
     // by an existing ID copied from elsewhere.
     CutObjectBase(int) : ObjectBase(-1) {}
+    // Constructor to initialize full information from 3mf
+    CutObjectBase(ObjectID id, size_t check_sum, size_t connectors_cnt) : ObjectBase(id), m_check_sum(check_sum), m_connectors_cnt(connectors_cnt) {}
 	// The class tree will have virtual tables and type information.
 	virtual ~CutObjectBase() = default;
 

src/slic3r/GUI/Gizmos/GLGizmoCut.cpp
@@ -627,7 +627,7 @@ void GLGizmoCut3D::render_cut_center_graber()
 
     const BoundingBoxf3 box = bounding_box();
 
-    const double mean_size = float((box.size().x() + box.size().y() + box.size().z()) / 6.0);
+    const double mean_size = float((box.size().x() + box.size().y() + box.size().z()) / 3.0);
     double size = m_dragging && m_hover_id == Z ? double(graber.get_dragging_half_size(mean_size)) : double(graber.get_half_size(mean_size));
 
     Vec3d cone_scale = Vec3d(0.75 * size, 0.75 * size, 1.8 * size);
@@ -821,8 +821,8 @@ bool GLGizmoCut3D::on_init()
 
 void GLGizmoCut3D::on_load(cereal::BinaryInputArchive& ar)
 {
-    ar( m_keep_upper, m_keep_lower, m_rotate_lower, m_rotate_upper, m_hide_cut_plane, m_mode, //m_selected,
-        m_connector_depth_ratio, m_connector_size, m_connector_mode, m_connector_type, m_connector_style, m_connector_shape_id,
+    ar( m_keep_upper, m_keep_lower, m_rotate_lower, m_rotate_upper, m_hide_cut_plane, m_mode, m_connectors_editing,//m_selected,
+ //       m_connector_depth_ratio, m_connector_size, m_connector_mode, m_connector_type, m_connector_style, m_connector_shape_id,
         m_ar_plane_center, m_ar_rotations);
 
     set_center_pos(m_ar_plane_center, true);
@@ -835,8 +835,8 @@ void GLGizmoCut3D::on_load(cereal::BinaryInputArchive& ar)
 
 void GLGizmoCut3D::on_save(cereal::BinaryOutputArchive& ar) const
 { 
-    ar( m_keep_upper, m_keep_lower, m_rotate_lower, m_rotate_upper, m_hide_cut_plane, m_mode, //m_selected,
-        m_connector_depth_ratio, m_connector_size, m_connector_mode, m_connector_type, m_connector_style, m_connector_shape_id,
+    ar( m_keep_upper, m_keep_lower, m_rotate_lower, m_rotate_upper, m_hide_cut_plane, m_mode, m_connectors_editing,//m_selected,
+ //       m_connector_depth_ratio, m_connector_size, m_connector_mode, m_connector_type, m_connector_style, m_connector_shape_id,
         m_ar_plane_center, m_ar_rotations);
 }
 
@@ -1224,7 +1224,10 @@ bool GLGizmoCut3D::update_bb()
         m_max_pos = box.max;
         m_min_pos = box.min;
         m_bb_center = box.center();
-        set_center_pos(m_bb_center + m_center_offset, true);
+        if (box.contains(m_center_offset))
+            set_center_pos(m_bb_center + m_center_offset, true);
+        else
+            set_center_pos(m_bb_center, true);
 
         m_radius = box.radius();
         m_grabber_connection_len = 0.75 * m_radius;// std::min<double>(0.75 * m_radius, 35.0);
@@ -1242,6 +1245,9 @@ bool GLGizmoCut3D::update_bb()
         m_circle.reset();
         m_scale.reset();
         m_snap_radii.reset();
+        m_reference_radius.reset();
+
+        on_unregister_raycasters_for_picking();
 
         if (CommonGizmosDataObjects::SelectionInfo* selection = m_c->selection_info()) {
             m_selected.clear();
@@ -1271,8 +1277,7 @@ void GLGizmoCut3D::on_render()
 {
     if (update_bb() || force_update_clipper_on_render) {
         update_clipper_on_render();
-        if (force_update_clipper_on_render)
-            m_c->object_clipper()->set_behavior(m_connectors_editing, m_connectors_editing, 0.4f);
+        m_c->object_clipper()->set_behavior(m_connectors_editing, m_connectors_editing, 0.4f);
     }
 
     init_picking_models();
@@ -1725,11 +1730,11 @@ void GLGizmoCut3D::perform_cut(const Selection& selection)
 
         const bool has_connectors = !mo->cut_connectors.empty();
         {
+            Plater::TakeSnapshot snapshot(plater, _L("Cut by Plane"));
             // update connectors pos as offset of its center before cut performing
             if (has_connectors && m_connector_mode == CutConnectorMode::Manual) {
                 m_selected.clear();
 
-                Plater::TakeSnapshot snapshot(plater, _L("Cut by Plane"));
                 for (CutConnector& connector : mo->cut_connectors) {
                     connector.rotation = rotation;
 

src/slic3r/GUI/Plater.cpp
@@ -5962,6 +5962,8 @@ void Slic3r::GUI::Plater::cut(size_t obj_idx, size_t instance_idx, const Vec3d& 
     // suppress to call selection update for Object List to avoid call of early Gizmos on/off update
     p->load_model_objects(new_objects, false, false);
 
+    this->allow_snapshots();
+
     // now process all updates of the 3d scene
     update();
     // Update InfoItems in ObjectList after update() to use of a correct value of the GLCanvas3D::is_sinking(),