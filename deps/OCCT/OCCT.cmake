2678dfeb20c0dfc470b4d2b2a076b7f09fc47f31
tamasmeszaros <meszaros.q@gmail.com>
2022-08-24 17:39:54 +0200

    Add patch to occt which removes installing of pdb files on msvc
    The path is not correctly deduced in the current version

--- deps/OCCT/OCCT.cmake
+++ deps/OCCT/OCCT.cmake
@@ -2,7 +2,9 @@ prusaslicer_add_cmake_project(OCCT
     #LMBBS: changed version to 7.6.2
     URL https://github.com/Open-Cascade-SAS/OCCT/archive/refs/tags/V7_6_2.zip
     URL_HASH SHA256=c696b923593e8c18d059709717dbf155b3e72fdd283c8522047a790ec3a432c5
+    # SOURCE_DIR c:/Users/mesza/Workspace/prusa3d/PrusaSlicer/dep_OCCT
 
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/occt_toolkit.cmake ./adm/cmake/
     CMAKE_ARGS
         -DINSTALL_DIR_LAYOUT=Unix # LMBBS
         -DBUILD_LIBRARY_TYPE=Static
@@ -21,7 +23,6 @@ prusaslicer_add_cmake_project(OCCT
 		-DBUILD_MODULE_Visualization=OFF
 )
 
-# TODO: debug mode does not compile
-# if (MSVC)
-#   add_debug_dep(dep_OCCT)
-# endif ()
+if (MSVC)
+  add_debug_dep(dep_OCCT)
+endif ()
