9dda5a84d554cd477017c0bd5f6d14d4d93bb6d5
Filip Sykala - NTB T15p <Filip.Sykala@prusa3d.cz>
2022-09-21 15:27:55 +0200

    Move object on bed - fix issue 36

--- src/slic3r/GUI/Jobs/EmbossJob.cpp
+++ src/slic3r/GUI/Jobs/EmbossJob.cpp
@@ -628,12 +628,8 @@ void priv::update_volume(TriangleMesh &&mesh,
         canvas->update_instance_printable_state_for_object((size_t) object_idx);
     
     // Move object on bed
-    if (GLGizmoEmboss::is_text_object(volume)) {
-        // Must be called after because GLVolume is invalidated by new_unique_id
-        plater->CallAfter([object_idx]() {
-            wxGetApp().plater()->canvas3D()->ensure_on_bed(object_idx, false);            
-        });
-    }
+    if (GLGizmoEmboss::is_text_object(volume))
+        volume->get_object()->ensure_on_bed();
 
     // redraw scene
     bool refresh_immediately = false;
