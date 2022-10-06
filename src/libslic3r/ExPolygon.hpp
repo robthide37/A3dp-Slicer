c86deb92db87231d53e549fc91ad97aef7592c5f
Pavel Mikus <pavel.mikus.mail@seznam.cz>
2022-10-06 12:19:33 +0200

    improve function name to reflect that it does unscaling (to_unscaled_linesf)

--- src/libslic3r/ExPolygon.hpp
+++ src/libslic3r/ExPolygon.hpp
@@ -125,7 +125,7 @@ inline Lines to_lines(const ExPolygons &src)
     return lines;
 }
 
-inline std::vector<Linef> to_linesf(const ExPolygons &src) 
+inline std::vector<Linef> to_unscaled_linesf(const ExPolygons &src) 
 {
     size_t n_lines = 0;
     for (ExPolygons::const_iterator it_expoly = src.begin(); it_expoly != src.end(); ++ it_expoly) {
@@ -138,9 +138,14 @@ inline std::vector<Linef> to_linesf(const ExPolygons &src)
     for (ExPolygons::const_iterator it_expoly = src.begin(); it_expoly != src.end(); ++ it_expoly) {
         for (size_t i = 0; i <= it_expoly->holes.size(); ++ i) {
             const Points &points = ((i == 0) ? it_expoly->contour : it_expoly->holes[i - 1]).points;
-            for (Points::const_iterator it = points.begin(); it != points.end()-1; ++it)
-                lines.push_back(Linef(unscaled(*it), unscaled(*(it + 1))));
-            lines.push_back(Linef(unscaled(points.back()), unscaled(points.front())));
+            Vec2d unscaled_a = unscaled(points.front());
+            Vec2d unscaled_b = unscaled_a;
+            for (Points::const_iterator it = points.begin()+1; it != points.end(); ++it){
+                unscaled_b = unscaled(*(it));
+                lines.push_back(Linef(unscaled_a, unscaled_b));
+                unscaled_a = unscaled_b;
+            }
+            lines.push_back(Linef(unscaled_a, unscaled(points.front())));
         }
     }
     return lines;
