19222137253a2ef453927820b15e3b646cfebc50
enricoturri1966 <enricoturri@seznam.cz>
2022-10-06 14:23:45 +0200

    Measuring: prototype for uniformly scale a volume by editing the value of the shown distance

--- src/slic3r/GUI/ImGuiWrapper.hpp
+++ src/slic3r/GUI/ImGuiWrapper.hpp
@@ -95,9 +95,6 @@ public:
 #if ENABLE_PREVIEW_LAYOUT
     bool draw_radio_button(const std::string& name, float size, bool active, std::function<void(ImGuiWindow& window, const ImVec2& pos, float size)> draw_callback);
 #endif // ENABLE_PREVIEW_LAYOUT
-    bool input_double(const std::string &label, const double &value, const std::string &format = "%.3f");
-    bool input_double(const wxString &label, const double &value, const std::string &format = "%.3f");
-    bool input_vec3(const std::string &label, const Vec3d &value, float width, const std::string &format = "%.3f");
     bool checkbox(const wxString &label, bool &value);
     void text(const char *label);
     void text(const std::string &label);
@@ -118,6 +115,7 @@ public:
     bool slider_float(const wxString& label, float* v, float v_min, float v_max, const char* format = "%.3f", float power = 1.0f, bool clamp = true, const wxString& tooltip = {}, bool show_edit_btn = true);
 
     bool image_button(ImTextureID user_texture_id, const ImVec2& size, const ImVec2& uv0 = ImVec2(0.0, 0.0), const ImVec2& uv1 = ImVec2(1.0, 1.0), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0.0, 0.0, 0.0, 0.0), const ImVec4& tint_col = ImVec4(1.0, 1.0, 1.0, 1.0), ImGuiButtonFlags flags = 0);
+    bool image_button(const wchar_t icon, const wxString& tooltip = L"");
 
     // Use selection = -1 to not mark any option as selected
     bool combo(const wxString& label, const std::vector<std::string>& options, int& selection, ImGuiComboFlags flags = 0);
