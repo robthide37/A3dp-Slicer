9ae91c291a9bfbc5cf7844118f0aa7088ca39925
Filip Sykala - NTB T15p <Filip.Sykala@prusa3d.cz>
2023-03-08 09:33:02 +0100

    misc

--- src/slic3r/GUI/Gizmos/GLGizmoSVG.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoSVG.cpp
@@ -119,10 +119,11 @@ bool GLGizmoSVG::on_mouse_for_translate(const wxMouseEvent &mouse_event)
     std::optional<double> up_limit;
     if (m_keep_up)
         up_limit = Slic3r::GUI::up_limit;
-    const Camera &camera       = wxGetApp().plater()->get_camera();
-    bool          was_dragging = m_surface_drag.has_value();
-    bool          res          = on_mouse_surface_drag(mouse_event, camera, m_surface_drag, m_parent, m_raycast_manager, up_limit);
-    bool          is_dragging  = m_surface_drag.has_value();
+    const Camera &camera = wxGetApp().plater()->get_camera();
+
+    bool was_dragging = m_surface_drag.has_value();
+    bool res = on_mouse_surface_drag(mouse_event, camera, m_surface_drag, m_parent, m_raycast_manager, up_limit);
+    bool is_dragging  = m_surface_drag.has_value();
 
     // End with surface dragging?
     if (was_dragging && !is_dragging) {
