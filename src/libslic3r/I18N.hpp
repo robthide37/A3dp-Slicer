73857703b8df3fcccd9dad28f0734d1cd9dec38b
Lukas Matena <lukasmatena@seznam.cz>
2023-03-15 09:21:12 +0100

    Added a check that libslic3r/I18N.hpp is not included on the frontend by mistake, also added a check that L macro is not defined when including libslic3r/I18N.hpp

--- src/libslic3r/I18N.hpp
+++ src/libslic3r/I18N.hpp
@@ -3,6 +3,12 @@
 
 #include <string>
 
+#ifdef SLIC3R_CURRENTLY_COMPILING_GUI_MODULE
+    #ifndef SLIC3R_ALLOW_LIBSLIC3R_I18N_IN_SLIC3R
+        #error You included libslic3r/I18N.hpp into a file belonging to slic3r module.
+    #endif
+#endif
+
 namespace Slic3r {
 
 namespace I18N {
@@ -15,11 +21,18 @@ namespace I18N {
 
 } // namespace Slic3r
 
-namespace {
-	const char* L(const char* s)            { return s; }
-	std::string _u8L(const char* s)         { return Slic3r::I18N::translate(s); }
-	std::string _utf8(const char* s)        { return Slic3r::I18N::translate(s); }
-	std::string _utf8(const std::string& s) { return Slic3r::I18N::translate(s); }
-}
+// When this is included from slic3r, better do not define the translation functions.
+// Macros from slic3r/GUI/I18N.hpp should be used there.
+#ifndef SLIC3R_CURRENTLY_COMPILING_GUI_MODULE
+	#ifdef L
+	    #error L macro is defined where it shouldn't be. Didn't you include slic3r/GUI/I18N.hpp in libslic3r by mistake?
+	#endif
+	namespace {
+		const char* L(const char* s)            { return s; }
+		std::string _u8L(const char* s)         { return Slic3r::I18N::translate(s); }
+		std::string _utf8(const char* s)        { return Slic3r::I18N::translate(s); }
+		std::string _utf8(const std::string& s) { return Slic3r::I18N::translate(s); }
+	}
+#endif
 
 #endif /* slic3r_I18N_hpp_ */
