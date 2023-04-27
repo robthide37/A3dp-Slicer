efe00f310b13d3e84ccf89d1cacf62986c2d95fb
enricoturri1966 <enricoturri@seznam.cz>
2023-04-27 08:55:59 +0200

    Fixed warnings

--- src/libslic3r/GCode/ConflictChecker.cpp
+++ src/libslic3r/GCode/ConflictChecker.cpp
@@ -32,7 +32,6 @@ inline Grids line_rasterization(const Line &line, int64_t xdist = RasteXDistance
     Point     rayStart     = line.a;
     Point     rayEnd       = line.b;
     IndexPair currentVoxel = point_map_grid_index(rayStart, xdist, ydist);
-    IndexPair firstVoxel   = currentVoxel;
     IndexPair lastVoxel    = point_map_grid_index(rayEnd, xdist, ydist);
 
     Point ray = rayEnd - rayStart;
