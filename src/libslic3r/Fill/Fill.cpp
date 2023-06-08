fd4ecb8db8bffaae4f501ee8fb052faceb768091
Lukas Matena <lukasmatena@seznam.cz>
2023-06-08 12:52:28 +0200

    Follow-up to previous commit: We also need to revert this part of 12f1cd0.

--- src/libslic3r/Fill/Fill.cpp
+++ src/libslic3r/Fill/Fill.cpp
@@ -653,11 +653,8 @@ Polylines Layer::generate_sparse_infill_polylines_for_anchoring(FillAdaptive::Oc
         f->print_config        = &this->object()->print()->config();
         f->print_object_config = &this->object()->config();
 
-        if (surface_fill.params.pattern == ipLightning) {
-            auto *lf            = dynamic_cast<FillLightning::Filler *>(f.get());
-            lf->generator       = lightning_generator;
-            lf->num_raft_layers = this->object()->slicing_parameters().raft_layers();
-        }
+        if (surface_fill.params.pattern == ipLightning)
+            dynamic_cast<FillLightning::Filler *>(f.get())->generator = lightning_generator;
 
         // calculate flow spacing for infill pattern generation
         double link_max_length = 0.;
