6f5fcf611d02bde5b1eea4fa1be2475cd05c44f5
YuSanka <yusanka@gmail.com>
2023-06-09 11:33:37 +0200

    Follow up https://github.com/Prusa-Development/PrusaSlicerPrivate/commit/ca3ee712fef0caa768cd595b1b0b12b11f08ca5b - Added comment line

--- src/slic3r/GUI/GUI_ObjectList.cpp
+++ src/slic3r/GUI/GUI_ObjectList.cpp
@@ -4382,8 +4382,8 @@ void ObjectList::update_and_show_object_settings_item()
     const wxDataViewItem item = GetSelection();
     if (!item) return;
 
+    // To get object item use GetTopParent(item). This function guarantees return of item with itObject type
     const wxDataViewItem obj_item = m_objects_model->GetTopParent(item);
-    assert(m_objects_model->GetItemType(obj_item) == ItemType::itObject);
     select_item([this, obj_item](){ return add_settings_item(obj_item, &get_item_config(obj_item).get()); });
 }
 
