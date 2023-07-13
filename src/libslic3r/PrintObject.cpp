4bb5d45ec19dd306e3d079e1b35b5c55e8cf9c89
Vojtech Bubnik <bubnikv@gmail.com>
2023-07-13 16:15:38 +0200

    ArcWelder: Integrated "arc_fitting" and "arc_fitting_tolerance" parameters to the UI and the slicing pipeline.

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -644,7 +644,9 @@ bool PrintObject::invalidate_state_by_config_options(
             || opt_key == "first_layer_extrusion_width"
             || opt_key == "perimeter_extrusion_width"
             || opt_key == "infill_overlap"
-            || opt_key == "external_perimeters_first") {
+            || opt_key == "external_perimeters_first"
+            || opt_key == "arc_fitting"
+            || opt_key == "arc_fitting_tolerance") {
             steps.emplace_back(posPerimeters);
         } else if (
                opt_key == "gap_fill_enabled"
--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -1648,6 +1648,8 @@ void TabPrint::build()
         optgroup->append_single_option_line("slicing_mode");
         optgroup->append_single_option_line("resolution");
         optgroup->append_single_option_line("gcode_resolution");
+        optgroup->append_single_option_line("arc_fitting");
+        optgroup->append_single_option_line("arc_fitting_tolerance");
         optgroup->append_single_option_line("xy_size_compensation");
         optgroup->append_single_option_line("elefant_foot_compensation", "elephant-foot-compensation_114487");
 
