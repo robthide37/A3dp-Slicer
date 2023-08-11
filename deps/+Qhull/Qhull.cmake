a8ba4f7ca9aa76741206edbfce8d11680ba5f3aa
tamasmeszaros <meszaros.q@gmail.com>
2023-08-11 15:42:07 +0200

    Fix build on windows
    Remove DEP_DEBUG feature on non MSVC platforms, it does not work nicely (due to Qhull)

--- deps/+Qhull/Qhull.cmake
+++ deps/+Qhull/Qhull.cmake
@@ -1,4 +1,7 @@
 include(GNUInstallDirs)
+# WARNING: Qhull does this nice thing to remove previous configs when installing.
+# It does that only on Unix. If you build release, then install, then build
+# debug, then install, it will delete the previous release config....
 add_cmake_project(Qhull
     URL "https://github.com/qhull/qhull/archive/v8.0.1.zip"
     URL_HASH SHA256=5287f5edd6a0372588f5d6640799086a4033d89d19711023ef8229dd9301d69b
