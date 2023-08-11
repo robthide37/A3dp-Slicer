c3ca460b7bcb40d1cd3402efd8c20141752e5252
tamasmeszaros <meszaros.q@gmail.com>
2023-08-11 10:07:32 +0200

    Add more boost libraries

--- deps/+Boost/Boost.cmake
+++ deps/+Boost/Boost.cmake
@@ -3,6 +3,6 @@ add_cmake_project(Boost
     URL_HASH SHA256=200f9292b5ef957ab551a648834239f502df165cb7bff18432702fb7ae98accb
     LIST_SEPARATOR |
     CMAKE_ARGS
-        -DBOOST_INCLUDE_LIBRARIES:STRING=system|iostreams|filesystem|thread|log|locale|regex|date_time|beast|nowide|polygon|uuid|math
+        -DBOOST_INCLUDE_LIBRARIES:STRING=system|iostreams|filesystem|thread|log|locale|regex|date_time|beast|nowide|polygon|uuid|math|geometry|foreach|multiprecision
         -DBUILD_TESTING:BOOL=OFF
 )
