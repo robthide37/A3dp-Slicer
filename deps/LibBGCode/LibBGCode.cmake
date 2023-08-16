e2472fc78cc04fdbd4f4377fd32ce1b6f1c24875
enricoturri1966 <enricoturri@seznam.cz>
2023-08-16 12:23:37 +0200

    Updated LibBGCode.cmake to download newer version of libbgcode

--- deps/LibBGCode/LibBGCode.cmake
+++ deps/LibBGCode/LibBGCode.cmake
@@ -1,8 +1,8 @@
 set(LibBGCode_SOURCE_DIR "" CACHE PATH "Optionally specify local LibBGCode source directory")
 
 set(_source_dir_line
-    URL https://github.com/prusa3d/libbgcode/archive/767a0970a0df277c366a08f32b72e39b6e628b54.zip
-    URL_HASH SHA256=60025397e9ce301ac925b59a12cfced26034b6124866b618c4771842d109847f
+    URL https://github.com/prusa3d/libbgcode/archive/49e7b47b7a9a7e9365613d3f90ad49d71a0e93d6.zip
+	URL_HASH SHA256=b9513ab2bbaf06a79c35c4d69226d211f1226ca625ce07c04f54b89b93e9bc75
 )
 
 if (LibBGCode_SOURCE_DIR)
