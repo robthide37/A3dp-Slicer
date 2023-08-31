5b1c97439fdfdc89245794582ace63c8267bd7e9
YuSanka <yusanka@gmail.com>
2023-08-31 09:01:56 +0200

    Partially revert of 695c08e0e70b5c85e26cfa96e57b6185270b93d0 (Redundant white-spaces was mistakenly added)

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -3579,9 +3579,9 @@ bool Plater::priv::replace_volume_with_stl(int object_idx, int volume_idx, const
         new_volume->convert_from_imperial_units();
     else if (old_volume->source.is_converted_from_meters)
         new_volume->convert_from_meters();
-        new_volume->supported_facets.assign(old_volume->supported_facets);
-        new_volume->seam_facets.assign(old_volume->seam_facets);
-        new_volume->mmu_segmentation_facets.assign(old_volume->mmu_segmentation_facets);
+    new_volume->supported_facets.assign(old_volume->supported_facets);
+    new_volume->seam_facets.assign(old_volume->seam_facets);
+    new_volume->mmu_segmentation_facets.assign(old_volume->mmu_segmentation_facets);
     std::swap(old_model_object->volumes[volume_idx], old_model_object->volumes.back());
     old_model_object->delete_volume(old_model_object->volumes.size() - 1);
     if (!sinking)
