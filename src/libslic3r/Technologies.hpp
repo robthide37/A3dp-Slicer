3a00a50ee520c42425a8ece88e76318533a5ee44
enricoturri1966 <enricoturri@seznam.cz>
2023-09-08 09:23:16 +0200

    Disabled tech ENABLE_BINARIZED_GCODE_DEBUG_WINDOW to hide imgui dialog to set binarized gcode parameters

--- src/libslic3r/Technologies.hpp
+++ src/libslic3r/Technologies.hpp
@@ -59,6 +59,6 @@
 #define ENABLE_OPENGL_DEBUG_OPTION (1 && ENABLE_GL_CORE_PROFILE)
 
 // Enable imgui dialog which allows to set the parameters used to export binarized gcode
-#define ENABLE_BINARIZED_GCODE_DEBUG_WINDOW 1
+#define ENABLE_BINARIZED_GCODE_DEBUG_WINDOW 0
 
 #endif // _prusaslicer_technologies_h_
