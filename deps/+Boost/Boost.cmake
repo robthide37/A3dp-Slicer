e1005f5dcc1b672e9fd493030e1d32320fff3264
tamasmeszaros <meszaros.q@gmail.com>
2023-10-25 11:46:59 +0200

    Fix for Boost build on Linux, broken by previous commit

--- deps/+Boost/Boost.cmake
+++ deps/+Boost/Boost.cmake
@@ -1,11 +1,13 @@
 
 set(_context_abi_line "")
+set(_context_arch_line "")
 if (APPLE AND CMAKE_OSX_ARCHITECTURES)
     if (CMAKE_OSX_ARCHITECTURES MATCHES "x86")
         set(_context_abi_line "-DBOOST_CONTEXT_ABI:STRING=sysv")
     elseif (CMAKE_OSX_ARCHITECTURES MATCHES "arm")
         set (_context_abi_line "-DBOOST_CONTEXT_ABI:STRING=aapcs")
     endif ()
+    set(_context_arch_line "-DBOOST_CONTEXT_ARCHITECTURE:STRING=${CMAKE_OSX_ARCHITECTURES}")
 endif ()
 
 add_cmake_project(Boost
@@ -15,6 +17,6 @@ add_cmake_project(Boost
     CMAKE_ARGS
         -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test
         -DBUILD_TESTING:BOOL=OFF
-        -DBOOST_CONTEXT_ARCHITECTURE:STRING=${CMAKE_OSX_ARCHITECTURES}
         "${_context_abi_line}"
+        "${_context_arch_line}"
 )
