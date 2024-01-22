7f423083b48b7bc7eae0d98fa4f359c629645e43
Martin Šach <martin.sachin@gmail.com>
2024-01-22 10:49:57 +0100

    Fix: Use toolchange z instead of writer z to restore z on wipe tower.
    Writer z can be set in custom toolchange gcode and than the restoration
    is invalid.

--- src/libslic3r/GCode/WipeTowerIntegration.cpp
+++ src/libslic3r/GCode/WipeTowerIntegration.cpp
@@ -93,7 +93,7 @@ std::string WipeTowerIntegration::append_tcr(GCodeGenerator &gcodegen, const Wip
             gcodegen.m_wipe.reset_path(); // We don't want wiping on the ramming lines.
         toolchange_gcode_str = gcodegen.set_extruder(new_extruder_id, tcr.print_z); // TODO: toolchange_z vs print_z
         if (gcodegen.config().wipe_tower) {
-            deretraction_str += gcodegen.writer().get_travel_to_z_gcode(z, "restore layer Z");
+            deretraction_str += gcodegen.writer().get_travel_to_z_gcode(tcr.print_z, "restore layer Z");
             deretraction_str += gcodegen.unretract();
         }
     }
