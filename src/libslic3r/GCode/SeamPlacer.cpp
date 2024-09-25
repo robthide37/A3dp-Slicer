ef89253588249a6b8b1e8c96b1bdc1e69685c6b4
supermerill <merill@free.fr>
2024-09-25 18:15:28 +0200

    finish fixing seam placer

--- src/libslic3r/GCode/SeamPlacer.cpp
+++ src/libslic3r/GCode/SeamPlacer.cpp
@@ -575,7 +575,7 @@ PolylineWithEnds extract_perimeter_polylines(const Layer *layer, const SeamPosit
                     visitor.also_overhangs = false;
                     if (polylines.empty()) {
                         // shouldn't happen
-                        assert(ex_entity->role() == ExtrusionRole::ThinWall); // no loops
+                        assert(ex_entity->role() == ExtrusionRole::ThinWall || perimeter_type == PerimeterGeneratorType::Arachne); // no loops
                         //ex_entity->visit(visitor);
                         // what to do in this case?
                         Points pts;
