bbe0481c8f9793e324cb9f1b66c734e568764cd8
stxve-cr3d <stefan.weiss1@aol.com>
2025-05-06 08:53:13 +0200

    only use webview edge if on MSVC

--- src/slic3r/GUI/A3DPWebView.cpp
+++ src/slic3r/GUI/A3DPWebView.cpp
@@ -314,6 +314,7 @@ wxWebView* WebView::CreateWebView(wxWindow * parent, wxString const & url)
     g_webviews.push_back(webView);
     return webView;
 }
+
 #if wxUSE_WEBVIEW_EDGE
 bool WebView::CheckWebViewRuntime()
 {
@@ -327,6 +328,7 @@ bool WebView::DownloadAndInstallWebViewRuntime()
     return DownloadAndInstallWV2RT() == 0;
 }
 #endif
+
 void WebView::LoadUrl(wxWebView * webView, wxString const &url)
 {
     auto url2  = url;
